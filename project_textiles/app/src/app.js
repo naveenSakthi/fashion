836f903fa591aa3c374581b41eee45f505b5dc4c
Naveen Sakthi <naveensftwr@gmail.com>
2015-03-11 22:04:30 +0530

    changes added

--- project_textiles/app/src/app.js
+++ project_textiles/app/src/app.js
@@ -1,9 +1,21 @@
-var myapp = angular.module('myapp', ["ui.router","ui.select2"])
+var myapp = angular.module('myapp', ["ui.router"])
     myapp.config(function($stateProvider){
     $stateProvider
         .state('index', {
             url: "/",
             templateUrl: 'src/views/home.tpl.html',
+        })
+        .state('category', {
+            url: "/category",
+            templateUrl: 'src/views/category.tpl.html',
+        })
+         .state('shops', {
+            url: "/shops",
+            templateUrl: 'src/views/fashion-shops.tpl.html',
+        })
+         .state('multi', {
+            url: "/multi",
+            templateUrl: 'src/views/multi-column.html',
         })
         
     })
